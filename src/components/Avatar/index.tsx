bb278a3274458b111ccb8d098bfd631d835d2035
Ian-Bright <ianbrighton1@gmail.com>
2022-02-18 15:42:14 -0700

    warnings removed

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,26 @@
+import makeBlockie from 'ethereum-blockies-base64';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  avatar: {
+    borderRadius: '50%',
+    height: '33px',
+    width: '33px',
+  },
+});
+
+type AvatarProps = {
+  address: string;
+  image?: string;
+};
+
+export default function Avatar({ address, image }: AvatarProps): JSX.Element {
+  const styles = useStyles();
+  return (
+    <img
+      alt={address}
+      className={styles.avatar}
+      src={image || makeBlockie(address)}
+    />
+  );
+}
