46e908d2a0189a96d9bf57ef1f1d79e62a9dc20a
Jack Gilcrest <me@jp4g.llc>
2022-03-18 16:05:00 -0400

    upgrade all dependencies to be up-to-date (react-scripts/ react was previously downgraded)

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/evmasm.js
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimcsponge.js
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimc7_gencontract.js
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/poseidon_gencontract.js
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimcsponge_gencontract.js
-sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimc7.js
-rm ./node_modules/circomlibjs/src/*.bak
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/evmasm.js
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimcsponge.js
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimc7_gencontract.js
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/poseidon_gencontract.js
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimcsponge_gencontract.js
+# sed -i'.bak' 's/import ethers/import { ethers }/g' ./node_modules/circomlibjs/src/mimc7.js
+# rm ./node_modules/circomlibjs/src/*.bak
