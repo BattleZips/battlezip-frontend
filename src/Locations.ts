ef3892a5fa6c8965fb83b58b057f6b1ee9c68dbc
Ian-Bright <ianbrighton1@gmail.com>
2022-10-09 16:49:09 -0500

    add redirect to resources route

--- src/Locations.ts
+++ src/Locations.ts
@@ -3,4 +3,5 @@ export const ActiveGameLocation = (id: string) => `/game/${id}`;
 export const JoinGameLocationTemplate = '/join-game/:id';
 export const JoinGameLocation = (id: string) => `/join-game/${id}`;
 export const NewGameLocation = '/new-game';
+export const ResourcesLocation = '/resources';
 export const RootLocation = '/';
